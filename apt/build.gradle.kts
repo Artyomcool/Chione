eb2e345d4f3603cf33d53953c1fb46bc7e7a4fcb
artyomcool <artyomcool@yandex-team.ru>
2018-08-07 04:29:55 +0300

    Better compile-testing usage

--- apt/build.gradle.kts
+++ apt/build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
 
     testCompile("junit", "junit", "4.12")
     testCompile("pl.pragmatists", "JUnitParams", "1.1.1")
-    testCompile("com.google.testing.compile", "compile-testing", "0.8")
+    testCompile("com.google.testing.compile", "compile-testing", "0.15")
     testCompile("com.google.guava", "guava", "19.0")
     testCompile("org.codehaus.groovy", "groovy-all", "2.4.6")
     testCompile("com.github.javaparser", "javaparser-core", "3.2.0")
--- apt/src/test/groovy/com/github/artyomcool/chione/AnnotationProcessorTest.groovy
+++ apt/src/test/groovy/com/github/artyomcool/chione/AnnotationProcessorTest.groovy
@@ -25,14 +25,15 @@
 package com.github.artyomcool.chione
 
 import com.github.javaparser.JavaParser
+import com.google.testing.compile.Compilation
+import com.google.testing.compile.Compiler
 import com.google.testing.compile.JavaFileObjects
 import junitparams.JUnitParamsRunner
 import junitparams.Parameters
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import javax.tools.*
-import java.nio.charset.Charset
+import javax.tools.StandardLocation
 
 @RunWith(JUnitParamsRunner)
 class AnnotationProcessorTest {
@@ -47,81 +48,27 @@ class AnnotationProcessorTest {
 
             JavaFileObjects.forSourceLines(fullName, code)
         }
-        def compiler = ToolProvider.getSystemJavaCompiler();
 
-        def generatedSources = []
-        def generatedClasses = []
+        def compilation = Compiler.javac()
+                .withProcessors(new AnnotationProcessor())
+                .compile(objects)
 
-        def stdFileManager = compiler.getStandardFileManager({}, Locale.ENGLISH, Charset.forName("UTF-8"))
-        JavaFileManager inMemoryFileManager = Class.forName("com.google.testing.compile.InMemoryJavaFileManager").newInstance(stdFileManager) as JavaFileManager
+        compilation.diagnostics().forEach({println it.getMessage(null)})
 
-        JavaFileManager fileManager = new ForwardingJavaFileManager(inMemoryFileManager) {
-            JavaFileObject getJavaFileForOutput(JavaFileManager.Location location, String className, JavaFileObject.Kind kind, FileObject sibling) {
-                switch (location.name) {
-                    case StandardLocation.SOURCE_OUTPUT.name:
-                        generatedSources << className
-                        break
-                    case StandardLocation.CLASS_OUTPUT.name:
-                        generatedClasses << className
-                        break
-                }
-                return super.getJavaFileForOutput(location, className, kind, sibling)
-            }
-        }
-
-        def diagnostics = []
-
-        def task = compiler.getTask(null, fileManager, { diagnostics << it }, [], [], objects);
-
-        task.setProcessors([new AnnotationProcessor()])
-
-        try {
-            if (!task.call()) {
-                throw new IllegalStateException("Compilation error");
-            }
-
-            ClassLoader loader = new ClassLoader(getClass().getClassLoader()) {
+        if (compilation.status() == Compilation.Status.SUCCESS) {
+            return new ClassLoader(getClass().getClassLoader()) {
                 @Override
                 Class<?> findClass(String name) throws ClassNotFoundException {
-                    if (name in generatedClasses) {
-                        def bytes = fileManager.getJavaFileForOutput(
-                                [getName: { 'CLASS_OUTPUT' }] as JavaFileManager.Location,
-                                name,
-                                JavaFileObject.Kind.CLASS,
-                                null
-                        ).openInputStream().bytes
-                        return defineClass(name, bytes, 0, bytes.length)
-                    }
-                    throw new ClassNotFoundException("Class not found: " + name)
+                    def className = name.replace(".", "/") + ".class"
+                    def file = compilation.generatedFile(StandardLocation.CLASS_OUTPUT, className)
+                            .orElseThrow({ new ClassNotFoundException("Class not found: " + name) })
+                    def bytes = file.openInputStream().bytes
+                    return defineClass(name, bytes, 0, bytes.length)
                 }
             }
-            def sources1 = getSources(inMemoryFileManager, generatedSources)
-            println sources1
-            println()
-            println diagnostics.join("\n")
-            println()
-
-            return loader
-        } catch (Exception e) {
-            def sources1 = getSources(inMemoryFileManager, generatedSources)
-            println sources1
-            println()
-            println diagnostics.join("\n")
-            println()
-            throw new IllegalStateException(e);
         }
 
-    }
-
-    static getSources(JavaFileManager fileManager, List<String> generatedSources) {
-        generatedSources.collect {
-            "====== File $it ======\n" + fileManager.getJavaFileForOutput(
-                    [getName: { 'SOURCE_OUTPUT' }] as JavaFileManager.Location,
-                    it,
-                    JavaFileObject.Kind.SOURCE,
-                    null
-            ).getCharContent(true)
-        }.join('\n\n')
+        throw new IllegalStateException("Compilation error")
     }
 
     ChioneModule<?, ?> generateModule(String name, String... sources) {
